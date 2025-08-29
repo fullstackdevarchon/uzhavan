d786c25af984c27c9be6ea148ba31031351f3d7a
Sudhakar-SP <sudhakars2609@gmail.com>
2025-08-29 23:13:00 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom"; // ✅ import BrowserRouter
-import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom"; // ✅ React Router for client-side routing
+import store from "./redux/store"; // ✅ Redux store
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Tailwind or global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* Provide Redux store to the app */}
     <Provider store={store}>
-      <BrowserRouter> {/* ✅ Wrap App in Router */}
+      {/* Wrap the entire app with BrowserRouter for routing */}
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
